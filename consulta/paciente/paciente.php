587fc085472d3cfa334233971fc36325ad3be2fe
Garcia <garcia-g@hotmail.com>
2024-04-18 16:34:49 -0300

    Att 18.04.2024

--- consulta/paciente/paciente.php
+++ consulta/paciente/paciente.php
@@ -38,16 +38,18 @@
             <form> 
             <table>
                 <tr>
-                <td class='nome'>Nome</td>
+                <td class='id'>Id</td>    
+                <td class='titulo' colspan='3'>Nome</td>
                 </tr>
                 <?php
                     include('../../conexao.php');
-                    $query="SELECT * FROM paciente ORDER BY nome";
+                    $query="SELECT * FROM paciente ORDER BY id";
                     $resu=mysqli_query($con,$query) or die(mysqli_connect_error());
                     while ($reg=mysqli_fetch_array($resu)) {
-                        echo "<tr><td class='nome'>".$reg['nome']."</td>";                        
-                        echo "<td><a href='alterar_paciente.php?id=".$reg['id']."'>Editar</a></td>";
-                        echo "<td><a href='excluir_paciente.php?id=".$reg['id']."'>Excluir</a></td></tr>";
+                        echo "<tr class='linha'><td class='id'>".$reg['id']."</td>";
+                        echo "<td class='nome'>".$reg['nome']."</td>";                        
+                        echo "<td class='editar'><a href='alterar_paciente.php?id=".$reg['id']."' class='txtEditEditar'>Editar</a></td>";
+                        echo "<td class='excluir'><a href='excluir_paciente.php?id=".$reg['id']."' class='txtEditExcluir'>Excluir 🗑</a></td></tr>";
                     }
                 ?>
             </table>    
